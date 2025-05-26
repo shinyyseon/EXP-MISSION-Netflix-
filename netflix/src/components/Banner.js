55c3dc7606bf594545546550d048316095d1dd2f
shinyyseon <epdlwl515@naver.com>
2025-05-26 18:08:50 +0900

    style: 배너 버튼 스타일 수정

--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -40,9 +40,15 @@ export default function Banner() {
                             className="banner_button play"
                             onClick={() => setIsClicked(true)}
                         >
-                            재생</button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="PlayStandard" aria-hidden="true">
+                                <path fill="currentColor" d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"></path>
+
+                            </svg> 재생</button>
                         <button className="banner_button info">
-                            <div className="space"></div> 상세 정보
+                            <div className="space"></div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="CircleIStandard" aria-hidden="true">
+                            <path fill="currentColor" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" clipRule="evenodd" fillRule="evenodd"></path>
+                        </svg> 상세 정보
                         </button>
                     </div>
                     <h1 className="banner_description">{truncate(movie.overview, 100)}</h1>
